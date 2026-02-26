9d89dacd43d2b16dbd66d811628d37c4482d2bcd
zx5g456 <435104670@qq.com>
2026-02-26 20:46:34 +1000

    Fix todo_by_id

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,jsonify
+from flask import Blueprint, jsonify, request
 
 bp = Blueprint('routes', __name__)
 
@@ -6,20 +6,8 @@ bp = Blueprint('routes', __name__)
 def health():
     return jsonify({'status': 'ok'})
 
-@bp.route('/api/v1/todos/<int:id>', methods=['GET'])
-def get_todos(id):
-    return jsonify([{
-            "id": id,
-            "title": "Watch CSSE6400 Lecture",
-            "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-            "completed": True,
-            "deadline_at": "2026-02-27T18:00:00",
-            "created_at": "2026-02-20T14:00:00",
-            "updated_at": "2026-02-20T14:00:00"
-        }])
-
-@bp.route('/api/v1/todos', methods=['POST'])
-def create_todo():
+@bp.route('/api/v1/todos', methods=['GET'])
+def get_todos():
     return jsonify([{
             "id": 1,
             "title": "Watch CSSE6400 Lecture",
@@ -30,9 +18,9 @@ def create_todo():
             "updated_at": "2026-02-20T14:00:00"
         }])
 
-@bp.route('/api/v1/todos/<int:id>', methods=['PUT'])
-def update_todo(id):
-    return jsonify([{
+@bp.route('/api/v1/todos/<int:id>', methods=['GET'])
+def test_get_todo_by_id(id):
+    return jsonify({
             "id": id,
             "title": "Watch CSSE6400 Lecture",
             "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
@@ -40,11 +28,35 @@ def update_todo(id):
             "deadline_at": "2026-02-27T18:00:00",
             "created_at": "2026-02-20T14:00:00",
             "updated_at": "2026-02-20T14:00:00"
-        }])
+        })
+
+@bp.route('/api/v1/todos', methods=['POST'])
+def create_todo():
+    return jsonify({
+            "id": 1,
+            "title": "Watch CSSE6400 Lecture",
+            "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+            "completed": True,
+            "deadline_at": "2026-02-27T18:00:00",
+            "created_at": "2026-02-20T14:00:00",
+            "updated_at": "2026-02-20T14:00:00"
+        }), 201
+
+@bp.route('/api/v1/todos/<int:id>', methods=['PUT'])
+def update_todo(id):
+    return jsonify({
+            "id": id,
+            "title": "Watch CSSE6400 Lecture",
+            "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+            "completed": True,
+            "deadline_at": "2026-02-27T18:00:00",
+            "created_at": "2026-02-20T14:00:00",
+            "updated_at": "2026-02-20T14:00:00"
+        })
 
 @bp.route('/api/v1/todos/<int:id>', methods=['DELETE'])
 def delete_todo(id):
-    return jsonify([{
+    return jsonify({
             "id": id,
             "title": "Watch CSSE6400 Lecture",
             "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
@@ -52,4 +64,4 @@ def delete_todo(id):
             "deadline_at": "2026-02-27T18:00:00",
             "created_at": "2026-02-20T14:00:00",
             "updated_at": "2026-02-20T14:00:00"
-        }])
+        })
